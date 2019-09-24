303c2653781a4c3fc7ea47ec97e4a7f6b3d51258
huntersgamedev <hunterfraser.gamedev@gmail.com>
2019-09-24 13:03:31 -0400

    updated everything with the servers IP

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -20,6 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+#have the secret in source code externally!
 SECRET_KEY = 'my%%1)$01y=fg%0*x5hij!lj=$2zf8czl@lg7wc-lvkqjmt7)_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -20,7 +20,7 @@ global target_url
 target_url = 'https://bbgbctest.blackboard.com'
 
 global redirect_url
-redirect_url='http://127.0.0.1:8000/info/'
+redirect_url='http://10.13.88.66:8000/info/'
 
 global SECRET
 SECRET = "ANuwg76VOV7aIA0KYqfv6KWIGOWEw0FX"  
@@ -35,7 +35,7 @@ KEY = "ce2cbd52-60e4-4e94-bb23-66099afe6d16"
 
 #TEMP STEP 1 of three legged OAuth opens a browser with working URL to get authorization code
 def redirecting(word):
-    webbrowser.open('https://bbgbctest.blackboard.com/learn/api/public/v1/oauth2/authorizationcode?redirect_uri=http://127.0.0.1:8000/info/&response_type=code&client_id=ce2cbd52-60e4-4e94-bb23-66099afe6d16&scope=read&state=DC1067EE-63B9-40FE-A0AD-B9AC069BF4B0/',new=0,autoraise=False)
+    webbrowser.open('https://bbgbctest.blackboard.com/learn/api/public/v1/oauth2/authorizationcode?redirect_uri=http://10.13.88.66:8000/info/&response_type=code&client_id=ce2cbd52-60e4-4e94-bb23-66099afe6d16&scope=read&state=DC1067EE-63B9-40FE-A0AD-B9AC069BF4B0/',new=0,autoraise=False)
     word=''
     
     return HttpResponse (word)
