9e5346a2f640022b9e22e44cfc621a172266e9ad
huntersgamedev <hunterfraser.gamedev@gmail.com>
2019-10-04 14:49:01 -0400

    WSGI windows edits

--- mysite/mysite/wsgi_windows.py
+++ mysite/mysite/wsgi_windows.py
@@ -1,4 +1,4 @@
-activate_this = 'C:\Users\101180612\Envs\cmApp\Scripts/activate_this.py'
+activate_this = "C:/Users/101180612/Envs/cmApp/Scripts/activate_this.py"
 # execfile(activate_this, dict(__file__=activate_this))
 exec(open(activate_this).read(),dict(__file__=activate_this))
 
@@ -7,14 +7,14 @@ import sys
 import site
 
 # Add the site-packages of the chosen virtualenv to work with
-site.addsitedir('C:\Users\101180612\Envs\cmApp\Lib\site-packages')
+site.addsitedir("C:/Users/101180612/Envs/cmApp/Lib/site-packages")
 
 
 
 
 # Add the app's directory to the PYTHONPATH
-sys.path.append('C:\Users\101180612\Envs\cmApp\CMserver\mysite')
-sys.path.append('C:\Users\101180612\Envs\cmApp\CMserver\mysite\mysite')
+sys.path.append("C:/Users/101180612/Envs/cmApp/CMserver/mysite")
+sys.path.append("C:/Users/101180612/Envs/cmApp/CMserver/mysite/mysite")
 
 os.environ['DJANGO_SETTINGS_MODULE'] = 'CMserver.settings'
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "CMserver.settings")
