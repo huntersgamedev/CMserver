15b91ad95232e1a319d827467e9bce79afe88aad
huntersgamedev <hunterfraser.gamedev@gmail.com>
2019-10-11 15:02:00 -0400

    trying to set up template path

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -40,7 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'scorsite.apps.ScorsiteConfig'
+    'scorsite.apps.ScorsiteConfig',
+    'mysite.apps.templates',
+
 ]
 
 MIDDLEWARE = [
